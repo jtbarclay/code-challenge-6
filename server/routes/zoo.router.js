59589552a5c57e527b19dc95b6a1278ef5e8b505
Jon Barclay <my@nameisjon.com>
2019-11-11 10:09:01 -0600

    router get route

--- server/routes/zoo.router.js
+++ server/routes/zoo.router.js
@@ -5,6 +5,20 @@ const pool = require('../modules/pool.js');
 router.get('/', (req, res) => {
     // YOUR CODE HERE
     
+    const query = `
+        SELECT s.species_name AS species, c.class_name AS "class" FROM species s
+        JOIN "class" c ON c.id=s.class_id;
+    `;
+
+    pool.query(query)
+        .then((response) => {
+            console.log('/zoo GET response', response);
+            res.send(response.rows);
+        })
+        .catch((error) => {
+            console.log('/zoo GET error', error);
+            res.sendStatus(500);
+        })
 });
 
 module.exports = router;
